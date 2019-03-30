d23756c2eeff89dd510024cf4956c9fab906a62d
ValdekBro <profittom5@gmail.com>
2019-03-30 22:15:40 +0200

    add filters

--- start/routes.js
+++ start/routes.js
@@ -15,6 +15,9 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const Post = use('App/Models/Post');
+
+
 Route.on    ('/signup').render('auth.signup');
 Route.on    ('/login').render('auth.login');
 
@@ -33,3 +36,21 @@ Route.post('/user_posts', 'PostController.create').as('CreatePost');
 Route.get('/user_posts/delete/:id', 'PostController.delete');
 Route.get('/user_posts/edit/:id', 'PostController.edit');
 Route.post('/posts/update/:id', 'PostController.update');
+
+
+Route.get('/ajax/posts_by_date', async ({ request, response }) => {
+    const posts = await Post.query()
+        .with('user')
+        .where('created_at', request.all().date)
+        .fetch();
+        return response.json({ posts: posts.toJSON() })
+});
+
+Route.get('/ajax/posts_by_user', async ({ request, response }) => {
+    const posts = await Post.query()
+        .with('user')
+        .where('user_id', 2)
+        .fetch();
+        
+        return response.json({ posts: posts.toJSON() })
+}); 
